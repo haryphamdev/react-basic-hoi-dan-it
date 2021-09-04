50e862be601b53aa2a52690a9281f867d2052b5c
Hary Pham <haryphamdev@gmail.com>
2021-09-04 22:06:27 +0700

    video4

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent.js';
+/**
+ * 2 components: class component / function component ( function, arrow)
+ * JSX
+ */
 
 function App() {
+  // const  App = () =>  {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello world with React.js (Hoi Dan IT)
+
         </p>
         <a
           className="App-link"
@@ -17,6 +24,8 @@ function App() {
         >
           Learn React
         </a>
+        <MyComponent />
+
       </header>
     </div>
   );
---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>hello my component Hoi Dan IT</div>
+        )
+
+    }
+}
+
+export default MyComponent;
