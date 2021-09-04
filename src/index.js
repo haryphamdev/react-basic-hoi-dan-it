0e40d9f6cffb2a40ec42f8af7b48905a63c3d1b5
Hary Pham <haryphamdev@gmail.com>
2021-09-04 15:37:28 +0700

    video3.3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
