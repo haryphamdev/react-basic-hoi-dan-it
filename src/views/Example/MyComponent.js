c3ac3f0239fb04fb634225ea0062514c422aabfc
Hary Pham <haryphamdev@gmail.com>
2021-09-05 10:33:38 +0700

    video5

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,38 @@ import React from 'react';
 
 class MyComponent extends React.Component {
 
+
+    //key:value
+    state = {
+        name: 'Ronaldo',
+        channel: 'Hoi Dan IT',
+    }
+
+    /* 
+    JSX => return block
+    fragment
+    */
+
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
+
         return (
-            <div>hello my component Hoi Dan IT</div>
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    My name is {this.state['name']}
+                </div>
+                <div className="second">
+                    My youtube channel : {this.state.channel}
+                </div>
+            </>
         )
 
     }
