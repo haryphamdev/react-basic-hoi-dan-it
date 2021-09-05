589a8e0b7456739b3a98546b0272c74e93313f44
Hary Pham <haryphamdev@gmail.com>
2021-09-05 17:54:19 +0700

    video8

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -5,7 +5,7 @@ class MyComponent extends React.Component {
 
     //key:value
     state = {
-        name: 'ERIC',
+        name: '',
         channel: 'Hoi Dan IT',
     }
 
@@ -16,18 +16,21 @@ class MyComponent extends React.Component {
 
 
     handleOnChangeName = (event) => {
+
+        // this.state.name = event.target.value;  //bad code
+        //merge
         this.setState({
-            name: event.target.value
+            name: event.target.value,
         })
     }
 
     handleClickButton = () => {
-        console.log('hit the button')
         alert('click me')
     }
 
+    //re-render
     render() {
-
+        console.log('>>> call render: ', this.state)
         return (
             <>
                 <div className="first">
