483a494f76de12e1aff17dc8361db2e7c05a2b6a
Hary Pham <haryphamdev@gmail.com>
2021-09-12 10:04:49 +0700

    video24

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
-
+import { withRouter } from "react-router";
 class Home extends React.Component {
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
+
+    //HOC: higher order component
     render() {
+        console.log('>>> check props: ', this.props)
         return (
             <div>
                 Hello world from Homepage with Eric & Hoi Dan IT
@@ -11,4 +19,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+export default withRouter(Home);
